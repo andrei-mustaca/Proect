76eae507469d3b4c840594d07a1c983a7ab1f7d0
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-11 07:42:40 +0200

    Create General.cs

---
+++ Курсовая/Курсовая/General.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курсовая
+{
+    class General
+    {
+        List<Hotel> numbers = new List<Hotel>();
+        List<Client> clients = new List<Client>();
+        List<Employee> employees = new List<Employee>();
+    }
+}
